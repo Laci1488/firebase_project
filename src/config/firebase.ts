525401ca3e27afa42e6d5f2c35a6a9850c9e33dd
Laci1488 <reimlaszlob@gmail.com>
2023-06-05 20:22:51 +0200

    Log in, log out, navbar

---
+++ src/config/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ00yB-2ngexKtKCDcdyqveSu9hFxZoOY",
+  authDomain: "first-firebase-project-1dbca.firebaseapp.com",
+  projectId: "first-firebase-project-1dbca",
+  storageBucket: "first-firebase-project-1dbca.appspot.com",
+  messagingSenderId: "1095346384109",
+  appId: "1:1095346384109:web:b353f95e8bc1665b2270e4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
